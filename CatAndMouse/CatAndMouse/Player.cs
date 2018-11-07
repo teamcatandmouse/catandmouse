b9113a7b1a3e67f06d9b4114738a6acfbdeeb18d
mikaelajayd <mikaelajayd@users.noreply.github.com>
2018-11-08 07:30:06 +0800

    Implemented basic movement

--- CatAndMouse/CatAndMouse/Player.cs
+++ CatAndMouse/CatAndMouse/Player.cs
@@ -17,11 +17,12 @@ namespace CatAndMouse
         Sprite playerSprite = new Sprite();
         
         MainGame game = null;
-        float playerSpeed = 175;
-        float playerRotateSpeed = 5;
+        float playerSpeed = 12500f;
+        //float playerMaxSpeed = 400f;
+       // float playerCurrentSpeed = 0f;
         Vector2 playerPosition = new Vector2(0, 0);
+        Vector2 playerVelocity = new Vector2(0, 0);
         Vector2 playerOffset = new Vector2(0, 0);
-        float playerAngle = 0;
         bool playerAlive = false;
 
         public Player()
@@ -32,16 +33,45 @@ namespace CatAndMouse
         public void Load(ContentManager content, MainGame theGame)
         {
             playerSprite.Load(content, "Mouse", true);
+
+            playerSprite.velocity = Vector2.Zero;
         }
 
         public void Update(float deltaTime)
         {
+            UpdateInput(deltaTime);
             playerSprite.Update(deltaTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             playerSprite.Draw(spriteBatch);
+        }
+
+        private void UpdateInput(float deltaTime)
+        {
+            Vector2 localAcceleration = new Vector2(0, 0);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) == true)
+            {
+                localAcceleration.Y = -playerSpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) == true)
+            {
+                localAcceleration.Y = playerSpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) == true)
+            {
+                localAcceleration.X = -playerSpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) == true)
+            {
+                localAcceleration.X = playerSpeed;
+            }
+
+            playerSprite.velocity = localAcceleration * deltaTime;
+            playerSprite.position += playerSprite.velocity * deltaTime;
+
         }
 
 
