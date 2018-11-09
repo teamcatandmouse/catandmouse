c65e3640ea6fae96155fb0f934f7fe9e2bfc5f3f
mikaelajayd <mikaelajayd@users.noreply.github.com>
2018-11-09 15:46:33 +0800

    Animations

--- CatAndMouse/CatAndMouse/Player.cs
+++ CatAndMouse/CatAndMouse/Player.cs
@@ -1,82 +1,115 @@
-﻿using System;
+﻿using CatAndMouse.Manager;
+using CatAndMouse.Models;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Audio;
 
 
 namespace CatAndMouse
 {
-   public class Player
+    public class Player
     {
-        Sprite playerSprite = new Sprite();
-        
-        MainGame game = null;
-        float playerSpeed = 12500f;
-        //float playerMaxSpeed = 400f;
-       // float playerCurrentSpeed = 0f;
-        Vector2 playerPosition = new Vector2(0, 0);
-        Vector2 playerVelocity = new Vector2(0, 0);
-        Vector2 playerOffset = new Vector2(0, 0);
-        bool playerAlive = false;
+        #region Fields
 
-        public Player()
+        protected AnimationManager _animationManager;
+
+        protected Dictionary<string, Animation> _animations;
+
+        protected Vector2 _position;
+
+        protected Texture2D _texture;
+
+        #endregion
+
+        #region Properties
+
+        private Input input;
+
+        public Vector2 Position
         {
-
-        }
-
-        public void Load(ContentManager content, MainGame theGame)
-        {
-            playerSprite.Load(content, "Mouse", true);
-
-            playerSprite.velocity = Vector2.Zero;
-        }
-
-        public void Update(float deltaTime)
-        {
-            UpdateInput(deltaTime);
-            playerSprite.Update(deltaTime);
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            playerSprite.Draw(spriteBatch);
-        }
-
-        private void UpdateInput(float deltaTime)
-        {
-            Vector2 localAcceleration = new Vector2(0, 0);
-
-            if (Keyboard.GetState().IsKeyDown(Keys.Up) == true)
+            get { return _position; }
+            set
             {
-                localAcceleration.Y = -playerSpeed;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Down) == true)
-            {
-                localAcceleration.Y = playerSpeed;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Left) == true)
-            {
-                localAcceleration.X = -playerSpeed;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Right) == true)
-            {
-                localAcceleration.X = playerSpeed;
-            }
+                _position = value;
 
-            playerSprite.velocity = localAcceleration * deltaTime;
-            playerSprite.position += playerSprite.velocity * deltaTime;
-
+                if (_animationManager != null)
+                    _animationManager.Position = _position;
+            }
         }
 
+        internal Input Input { get => input; set => input = value; }
 
+        public float Speed = 3f;
 
+        public Vector2 Velocity;
+        internal Vector2 position;
 
+        #endregion
 
+        #region Methods
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if (_texture != null)
+                spriteBatch.Draw(_texture, Position, Color.White);
+            else if (_animationManager != null)
+                _animationManager.Draw(spriteBatch);
+            else throw new Exception("Not Good.");
+        }
+
+        public virtual void Move()
+        {
+            if (Keyboard.GetState().IsKeyDown(Input.Up))
+                Velocity.Y = -Speed;
+            else if (Keyboard.GetState().IsKeyDown(Input.Down))
+                Velocity.Y = Speed;
+            else if (Keyboard.GetState().IsKeyDown(Input.Left))
+                Velocity.X = -Speed;
+            else if (Keyboard.GetState().IsKeyDown(Input.Right))
+                Velocity.X = Speed;
+        }
+
+        protected virtual void SetAnimations()
+        {
+            if (Velocity.X > 0)
+                _animationManager.Play(_animations["WalkRight"]);
+            else if (Velocity.X < 0)
+                _animationManager.Play(_animations["WalkLeft"]);
+            else if (Velocity.Y > 0)
+                _animationManager.Play(_animations["WalkDown"]);
+            else if (Velocity.Y < 0)
+                _animationManager.Play(_animations["WalkUp"]);
+            else _animationManager.Stop();
+        }
+
+        public Player(Dictionary<string, Animation> animations)
+        {
+            _animations = animations;
+            _animationManager = new AnimationManager(_animations.First().Value);
+        }
+
+        public Player(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public virtual void Update(GameTime gameTime, List<Player> sprites)
+        {
+            Move();
+
+            SetAnimations();
+
+            _animationManager.Update(gameTime);
+
+            Position += Velocity;
+            Velocity = Vector2.Zero;
+        }
+
+        #endregion
     }
 }
