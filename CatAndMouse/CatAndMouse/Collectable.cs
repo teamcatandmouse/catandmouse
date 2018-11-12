f66020417797474e317e83901a01901ae528cfb6
liamhamilton <43400952+liamhamilton@users.noreply.github.com>
2018-11-12 19:37:42 +1100

    done collectable.cs

---
+++ CatAndMouse/CatAndMouse/Collectable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace CatAndMouse
+{
+	public class Collectable
+	{
+		public Sprite collectSprite = new Sprite();
+		Collision collision = new Collision();
+		MainGame game = null;
+
+		public void Load(ContentManager content, MainGame theGame)
+		{
+			collectSprite.Load(content, "Cheese", true);
+			game = theGame;
+
+			
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			collectSprite.Draw(spriteBatch);
+		}
+		public void Update(float deltaTime)
+		{
+			collision.game = game;
+			collectSprite.UpdateHitBox();
+			
+		}
+	}
+}
