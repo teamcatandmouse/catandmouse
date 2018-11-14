8651b7083fdca500a65d4fc36db29cb6c73489b6
DanBeaumont <daniel.beaumont@aie.edu.au>
2018-11-14 18:39:55 +1030

    re-added enemy.cs
    updated sprite class to be public
    readded missing enemy class

---
+++ CatAndMouse/CatAndMouse/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace CatAndMouse
+{
+    public class Enemy
+    {
+        public Sprite enemySprite = new Sprite();
+
+        Texture2D texture;
+
+        float walkSpeed = 7500f;
+        Collision collisions = new Collision();
+        MainGame game = null;
+
+        public void Load(ContentManager content, MainGame game)
+        {
+            enemySprite.Load(content, "BCat", true);
+            this.game = game;
+
+            enemySprite.velocity = Vector2.Zero;
+            //enemySprite.position = new Vector2(game.GraphicsDevice.Viewport.Width / 2, 0);       
+
+        }
+
+        public void Update(float deltaTime)
+        {
+            collisions.game = game;
+            enemySprite.Update(deltaTime);
+            enemySprite.UpdateHitBox();
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            enemySprite.Draw(spriteBatch);
+        }
+
+
+
+    }
+}
