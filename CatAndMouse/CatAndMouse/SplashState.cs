66ae6f6defdbe782795ae58f6247966f9e43bc29
JustinMcGrath01 <43329308+JustinMcGrath01@users.noreply.github.com>
2018-11-19 18:55:54 +1100

    States Added
    Added SplashState
    Added GameState
    Added GameOverState

---
+++ CatAndMouse/CatAndMouse/SplashState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Platformer
+{
+   public class SplashState : AIE.State
+   {
+        SpriteFont font = null;
+        float timer = 3;
+            
+        public SplashState() : base()
+        {
+
+        }
+
+        public override void Update(ContentManager content, GameTime gameTime)
+        { if (font == null)
+            { font = content.Load<SpriteFont>("Arial"); } timer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (timer <= 0)
+            {
+                AIE.StateManager.ChangeState("GAME");
+                timer = 3;
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(); spriteBatch.DrawString(font, "Splash", new Vector2(200, 200), Color.White); spriteBatch.End();
+        }
+
+        public override void CleanUp()
+        {
+            font = null;
+            timer = 3;
+        }
+
+   }
+}
